68c7659055b0481b109e9d9cae8e7f546863c3a5
sistemasiq <Argus@DESKTOP-RUUR7E8>
2018-04-21 11:18:18 -0500

    Commit inicial

---
+++ TurboERP_backend/src/main/java/com/turbomaquinas/TurboErpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.turbomaquinas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurboErpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurboErpBackendApplication.class, args);
+	}
+	
+}
