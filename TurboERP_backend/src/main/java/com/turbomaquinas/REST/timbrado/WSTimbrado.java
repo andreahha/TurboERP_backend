5d594f56c2bc24f4a71d8ea3d35d7ce32f38e007
Admin <Admin@DESKTOP-EFDTCLP>
2018-06-15 10:54:30 -0500

    Correción timbrado

--- TurboERP_backend/src/main/java/com/turbomaquinas/REST/timbrado/WSTimbrado.java
+++ TurboERP_backend/src/main/java/com/turbomaquinas/REST/timbrado/WSTimbrado.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -82,7 +83,34 @@ public class WSTimbrado {
 		    JSONObject json_AckEnlaceFiscal = new JSONObject(AckEnlaceFiscal);
 		    String estatusDocumento=(String) json_AckEnlaceFiscal.getString("estatusDocumento");
 		    if(estatusDocumento.equalsIgnoreCase("aceptado")){
-		    	return lt.timbrar(ffs.obtenerJSONFacturaFinal(id, modo));
+		    	return new ResponseEntity<String>(response.getBody(),HttpStatus.OK);
+		    }else if(estatusDocumento.equalsIgnoreCase("rechazado")){
+		    	return new ResponseEntity<String>(response.getBody(),HttpStatus.NOT_ACCEPTABLE);
+		    }else{
+		    	return null;
+		    }
+        }catch(Exception e){return new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);}
+        
+	}
+	
+	@GetMapping("/facturafinal/{id}")
+	public ResponseEntity<String> buscarFacturaFinal(@PathVariable int id,@RequestParam String modo) throws JsonParseException, JsonMappingException, IOException{
+		//Recuperar JSON del PA TIMBRADO_FACTURA		
+		String cfdi=null;
+		try{
+			cfdi=ffs.obtenerJSONFacturaFinal(id,modo);//mandar modo
+		}catch(DataAccessException e){
+			bitacora.error(e.getMessage());
+			return new ResponseEntity<String>(HttpStatus.CONFLICT);
+		}
+        try{
+        	ResponseEntity<String> response=lt.timbrar(ffs.obtenerJSONFacturaFinal(id, modo));
+	        JSONObject jsonRespuesta = new JSONObject(response.getBody());
+	        String AckEnlaceFiscal=(String) jsonRespuesta.getString("AckEnlaceFiscal");
+		    JSONObject json_AckEnlaceFiscal = new JSONObject(AckEnlaceFiscal);
+		    String estatusDocumento=(String) json_AckEnlaceFiscal.getString("estatusDocumento");
+		    if(estatusDocumento.equalsIgnoreCase("aceptado")){
+		    	return new ResponseEntity<String>(response.getBody(),HttpStatus.OK);
 		    }else if(estatusDocumento.equalsIgnoreCase("rechazado")){
 		    	return new ResponseEntity<String>(response.getBody(),HttpStatus.NOT_ACCEPTABLE);
 		    }else{
