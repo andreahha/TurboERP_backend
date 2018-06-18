5837d9a5323b87b0ae3d8b0fc21f0597d5dace1c
javi.villanueva <javi.villanueva@JVILLANUEVA.LP.TURBOMAQUINAS.CORP>
2018-06-18 08:54:56 -0500

    Se Agrego un nuevo webservice para hacer un cierre de mes que afecta la tabla de PARAMETROS

--- TurboERP_backend/src/main/java/com/turbomaquinas/DAO/general/ParametroDAO.java
+++ TurboERP_backend/src/main/java/com/turbomaquinas/DAO/general/ParametroDAO.java
@@ -7,5 +7,7 @@ import com.turbomaquinas.POJO.general.Parametro;
 public interface ParametroDAO {
 	
 	public List<Parametro> consultar();
+	public Parametro buscar(int id);
+	public Parametro cierreMes (Parametro parametro);
 
 }
